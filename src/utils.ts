28b9da883ff2d825abb2677741efcb6eea646a1d
Hegyi Áron Ferenc <36823200+Rettend@users.noreply.github.com>
2024-08-20 19:17:39 +0200

    feat: loading indicator

--- src/utils.ts
+++ src/utils.ts
@@ -1,32 +1,51 @@
 import path from 'node:path'
 import process from 'node:process'
 import { Console } from 'node:console'
+import c from 'picocolors'
 
 const cwd = process.env.INIT_CWD || process.cwd()
 export const r = (file: string) => path.join(cwd, file)
-
-const styles = {
-  cyan: '\x1B[36m',
-  bold: '\x1B[1m',
-  italic: '\x1B[3m',
-  underline: '\x1B[4m',
-  reset: '\x1B[0m',
-}
 
 function formatLog(str: unknown) {
   if (typeof str !== 'string')
     return str
   return str
-    .replace(/`([^`]+)`/g, (_, p1) => `${styles.cyan}${p1}${styles.reset}`)
-    .replace(/\*\*([^*]+)\*\*/g, (_, p1) => `${styles.bold}${p1}${styles.reset}`)
-    .replace(/\*([^*]+)\*/g, (_, p1) => `${styles.italic}${p1}${styles.reset}`)
-    .replace(/__([^_]+)__/g, (_, p1) => `${styles.underline}${p1}${styles.reset}`)
+    .replace(/`([^`]+)`/g, (_, p1) => `${c.cyan(p1)}`)
+    .replace(/\*\*([^*]+)\*\*/g, (_, p1) => `${c.bold(p1)}`)
+    .replace(/\*([^*]+)\*/g, (_, p1) => `${c.italic(p1)}`)
+    .replace(/__([^_]+)__/g, (_, p1) => `${c.underline(p1)}`)
+}
+
+export function createLoadingIndicator(message: string) {
+  const frames = ['⠋', '⠙', '⠹', '⠸', '⠼', '⠴', '⠦', '⠧', '⠇', '⠏']
+  let i = 0
+  const interval = setInterval(() => {
+    process.stdout.clearLine(0)
+    process.stdout.cursorTo(0)
+    process.stdout.write(`${c.yellow(frames[i])} ${message}`)
+    i = (i + 1) % frames.length
+  }, 100)
+
+  return () => {
+    clearInterval(interval)
+    process.stdout.clearLine(0)
+    process.stdout.cursorTo(0)
+  }
 }
 
 class ConsoleExtended extends Console {
   log(...data: any[]): void
   log(message?: unknown, ...optionalParams: unknown[]): void {
     super.log(formatLog(message), ...optionalParams)
+  }
+
+  loading(message: string, callback: () => void) {
+    const stopLoading = createLoadingIndicator(message)
+
+    setTimeout(() => {
+      stopLoading()
+      callback()
+    }, 3000)
   }
 }
 
